77a6fa64f96761e69dd06ff913ad1cb181c44798
hyejeongkk <49331209+hyejeongkk@users.noreply.github.com>
2019-05-25 13:05:23 +0900

    HomePresenter and Section Components 수정

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,39 @@
-export default()  => "Home";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Section from 'Components/Section';
+
+const Container = styled.div`
+    padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) => 
+    loading ? null :  (
+        <Container>
+            {nowPlaying && nowPlaying.length > 0 && (
+                <Section title="Now playing">
+                    {nowPlaying.map(movie => movie.title)}
+                </Section>
+            )}
+            {upcoming && upcoming.length > 0 && (
+                <Section title="Upcoming Movies">
+                    {nowPlaying.map(movie => movie.title)}
+                </Section>
+            )}
+            {popular && popular.length > 0 && (
+                <Section title="Popular Movies">
+                    {nowPlaying.map(movie => movie.title)}
+                </Section>
+            )}
+        </Container>
+    );
+
+HomePresenter.prototype = {
+    nowPlaying:PropTypes.array,
+    popular:PropTypes.array,
+    upcoming:PropTypes.array,
+    loading:PropTypes.bool.isRequired,
+    error:PropTypes.string
+};
+
+export default HomePresenter;
